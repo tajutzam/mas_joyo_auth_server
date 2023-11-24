8e42373b2a51a2540ae8c3731b6b4c2c90e61860
tajutzam <mohammadtajutzamzami07@gmail.com>
2023-11-24 15:09:36 +0700

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthController extends Controller
+{
+    //
+    public function login()
+    {
+
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function loginPost(Request $request)
+    {
+        $googleUser = Socialite::driver('google')->user();
+        $user = User::updateOrCreate([
+            'google_id' => $googleUser->id,
+        ], [
+            'name' => $googleUser->name,
+            'email' => $googleUser->email,
+            'google_token' => $googleUser->token,
+            'google_refresh_token' => $googleUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+        return redirect()->intended();
+    }
+}
